6c5340606852cb695152fc86e646f3fd81b74cc7
fox <d1248959@o365.fcu.edu.tw>
2025-03-04 00:48:45 +0800

    say cold jokes

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById('reload').addEventListener('click', function() {
+document.getElementById('reload').addEventListener('click', () => {
     location.reload();
 });
+
+const jokes = [
+    "What’s a computer’s favorite drink in winter?<br> → Hot Java.",
+    "Why was the server shivering?<br> → Because it was in a cold storage.",
+    "Why was the IT guy cold?<br> → Because he left his Windows open.",
+    "Why don’t programmers like nature?<br> → It has too many bugs.",
+    "Why was the function so sad?<br> → Because it didn’t return anything."
+];
+
+document.getElementById('joke_content').innerHTML = jokes[Math.floor(Math.random() * jokes.length)];
